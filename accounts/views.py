382019d552c5dfef472dfe8e293f51d745e00ad0
PratikaNG <pratika.05ng@gmail.com>
2023-07-11 15:48:55 +0530

    registration page

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth.forms import UserCreationForm
 
 def home(request):
     return render(request,'index.html')
 def treasure(request):
     return render(request,'treasure.html')
+
+def register(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+    else:
+        form = UserCreationForm()
+    return render(request,'registration/register.html',{'form':form})
