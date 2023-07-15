f90454bfe7be78f10e7957113e27071bd407cadb
Pratika NG <pratika.05ng@gmail.com>
2023-07-15 20:08:32 +0530

    login requirements and navbar improvisations and finishing up

--- accounts/views.py
+++ accounts/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render,redirect
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
 
 def home(request):
     return render(request,'index.html')
+
+@login_required
 def treasure(request):
     return render(request,'treasure.html')
 
